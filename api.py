2a71b27cae795a1f966f0b4913210ebbc4b58438
algnot <thanawat.tongla@gmail.com>
2025-03-23 20:54:40 +0700

    feat: fix session to global

--- api.py
+++ api.py
@@ -3,9 +3,10 @@ import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
-
+session = None
 
 def login():
+    global session
     session = requests.Session()
     login_url = "https://www.erawancenter.bangkok.go.th/login"
 
@@ -36,18 +37,22 @@ def login():
     response = session.post(login_url, headers=headers, data=payload, allow_redirects=True)
 
     if "laravel_session" in session.cookies:
+        print("Login successful!")
         return session
     else:
+        print("Login failed!")
         return None
 
 
-@app.route('/')
-def playground():
-    session = login()
-    if session is None:
-        return jsonify({"message": "Login failed. Check credentials."}), 401
+def get_data():
+    global session
 
-    data_url = "https://www.erawancenter.bangkok.go.th/command-datatable?draw=1&columns%5B0%5D%5Bdata%5D=RC_DATE&columns%5B1%5D%5Bdata%5D=RC_TIME&columns%5B2%5D%5Bdata%5D=RC_POINT&columns%5B3%5D%5Bdata%5D=CAL_TEL&columns%5B4%5D%5Bdata%5D=RC_PRIMARYILLNESS&columns%5B5%5D%5Bdata%5D=USER_CALLER&order%5B0%5D%5Bcolumn%5D=0&order%5B0%5D%5Bdir%5D=desc&order%5B1%5D%5Bcolumn%5D=1&order%5B1%5D%5Bdir%5D=desc&start=0&length=10&tab=calltaker&type=calltaker"
+    if session is None:
+        session = login()
+        if session is None:
+            return {"message": "Login failed. Check credentials."}, 401
+
+    data_url = "https://www.erawancenter.bangkok.go.th/command-datatable?draw=1&columns%5B0%5D%5Bdata%5D=RC_DATE&columns%5B1%5D%5Bdata%5D=RC_TIME&columns%5B2%5D%5D=RC_POINT&columns%5B3%5D%5D=CAL_TEL&columns%5B4%5D%5D=RC_PRIMARYILLNESS&columns%5B5%5D%5D=USER_CALLER"
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
@@ -56,12 +61,23 @@ def playground():
 
     response = session.get(data_url, headers=headers)
 
-    if response.status_code == 200:
-        return jsonify(response.json())
-    else:
-        return jsonify(
-            {"message": "Failed to retrieve data", "status_code": response.status_code}), response.status_code
+    if response.status_code in [401, 403] or "login" in response.url:
+        print("Session expired, re-logging in...")
+        session = login()
+        if session is None:
+            return {"message": "Re-login failed. Check credentials."}, 401
 
+        response = session.get(data_url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json(), 200
+    else:
+        return {"message": "Failed to retrieve data", "status_code": response.status_code}, response.status_code
+
+@app.route('/')
+def playground():
+    data, status_code = get_data()
+    return jsonify(data), status_code
 
 if __name__ == '__main__':
     app.run(debug=True)
